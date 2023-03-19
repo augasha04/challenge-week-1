6448c5c52a67092bf454825c935335d6c9e7b99e
augasha04 <126458610+augasha04@users.noreply.github.com>
2023-03-19 20:06:42 +0300

    Update chall3.js

--- chall3.js
+++ chall3.js
@@ -1,3 +1,6 @@
+// create functions for the deductions and gross salary
+
+//calculate gross/basic salary
 function netSalary (basic_salary, benefits){
     Payee = findPayee (basic_salary);
        console.log (Payee);
@@ -12,6 +15,7 @@ function netSalary (basic_salary, benefits){
     return netSalary;
 }
 
+// calculate payee
 function findPayee (basic_salary){
 let Payee = 0;
     if (basic_salary <= 288000){
@@ -30,6 +34,7 @@ let Payee = 0;
 }
         //console.log (findPayee);
 
+// calculate the deductions starting with NHIF
 function findNHIF (basic_salary){
 let NHIF = 0;
     if (basic_salary >= 0 && basic_salary <= 5999){
@@ -104,7 +109,7 @@ let NHIF = 0;
 }
             //console.log (findNHIF);
 
-
+// calculate NSSF deductions
 function findNSSF (basic_salary){
 let NSSF = 0;
     if (basic_salary <= 72000){
